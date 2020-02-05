d23095a5e2f02c7cfe74082dd133ee826033dca0
Ashutosh Ranjan <ashutosh.ranjan@hotelsoft.com>
2020-02-05 12:18:21 +0530

    Added test cases for aioodbc backend

--- tests/test_connection_options.py
+++ tests/test_connection_options.py
@@ -5,6 +5,7 @@ Unit tests for the backend connection arguments.
 from databases.backends.aiopg import AiopgBackend
 from databases.backends.mysql import MySQLBackend
 from databases.backends.postgres import PostgresBackend
+from databases.backends.mssql import MSSQLBackend
 
 
 def test_postgres_pool_size():
@@ -81,3 +82,26 @@ def test_aiopg_explicit_ssl():
     backend = AiopgBackend("postgresql+aiopg://localhost/database", ssl=True)
     kwargs = backend._get_connection_kwargs()
     assert kwargs == {"ssl": True}
+def test_aioodbc_pool_size():
+    backend = MSSQLBackend("aioodbc+asyncpg://localhost/test_database1?min_size=1&max_size=20")
+    kwargs = backend._get_connection_kwargs()
+    assert kwargs["min_size"] == 1
+    assert kwargs["max_size"] == 20
+
+def test_aioodbc_explicit_pool_size():
+    backend = PostgresBackend("aioodbc+asyncpg://localhost/test_database1", min_size=1, max_size=20)
+    kwargs = backend._get_connection_kwargs()
+    assert kwargs["min_size"] == 1
+    assert kwargs["max_size"] == 20
+
+
+def test_aioodbc_ssl():
+    backend = PostgresBackend("aioodbc+asyncpg://localhost/test_database1?ssl=true")
+    kwargs = backend._get_connection_kwargs()
+    assert kwargs["ssl"] == True
+
+
+def test_aioodbc_explicit_ssl():
+    backend = PostgresBackend("aioodbc+asyncpg://localhost/test_database1", ssl=True)
+    kwargs = backend._get_connection_kwargs()
+    assert kwargs["ssl"] == True
