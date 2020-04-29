85149a1bb315c0a88e7ef654dede1dc07f6237f7
blthree <blthree89@gmail.com>
2020-04-29 15:22:28 -0400

    add aioodbc driver to mssql in SUPPORTED_BACKENDS

--- databases/core.py
+++ databases/core.py
@@ -47,7 +47,7 @@ class Database:
         "postgres": "databases.backends.postgres:PostgresBackend",
         "mysql": "databases.backends.mysql:MySQLBackend",
         "sqlite": "databases.backends.sqlite:SQLiteBackend",
-        "mssql": "databases.backends.mssql:MSSQLBackend"
+        "mssql+aioodbc": "databases.backends.mssql:MSSQLBackend"
     }
 
     def __init__(
